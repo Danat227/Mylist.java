3d4dd5acca27c3fdfedf7ac46207b44d35d4f83b
danatbek <danatbek09876@gmail.com>
2026-04-09 20:19:20 +0500

    Now im done MyArrayList with all methods

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,39 @@ public class Main {
 //        list.add("Apple");
 //        list.get(0);
 //        System.out.println(list.get(0));
-     list.addLast("Camry");
-     list.addLast("Bmw");
-     ;
-        System.out.println(list.get(0));
-        System.out.println(list.get(1));
+//     list.addLast("Camry");
+//     list.addLast("Bmw");
+//     ;
+//        System.out.println(list.get(0));
+//        System.out.println(list.get(1));
+//
+//        System.out.println("Size: " + list.size());
+//       list.add("1");
+//       list.add("5");
+//       list.add("7");   // { 1, 2, 3, 4}
+//        list.add("3");
+//        list.add("3");
+//
+//        list.add("3");
+//
+//       list.remove(3);
+//        System.out.println(list.size());
+//        System.out.println("элемент под индексом 1: " + list.get(1));
+//        list.add("bmw");
+//        list.add("lexus");
+//        list.add("lada");
+//        System.out.println(list.indexOf("bmw"));
+//        System.out.println(list.lastIndexOf("lada"));
+//        System.out.println(list.exists("lexus"));
+        list.add("C");
+        list.add("A");
+        list.add("B");
 
-        System.out.println("Size: " + list.size());
+        System.out.println("До: " + list.get(0) + list.get(1) + list.get(2));
+
+        list.sort();
+
+        System.out.println("После: " + list.get(0) + list.get(1) + list.get(2));
 
 
 
